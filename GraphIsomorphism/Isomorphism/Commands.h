46fc96e65e21cf0e5a395888fa38b383fb821698
Wolzev <wolzev@mail.ru>
2017-06-07 02:56:40 +0500

    random isomorphic graphs

--- GraphIsomorphism/Isomorphism/Commands.h
+++ GraphIsomorphism/Isomorphism/Commands.h
@@ -9,6 +9,7 @@ namespace isomorphism
 	void help();
 	void stop();
 	void load(Graph& graph1, Graph& graph2);
+	void rig(Graph& graph1, Graph& graph2, int n);
 	void show(const Graph& graph1, const Graph& graph2);
 	void check(const Graph& graph1, const Graph& graph2);
 }
